89511122f64c4691ada7139226dcd54251a6418a
Devesh Mittal <99072096+DeveshM2002@users.noreply.github.com>
2024-10-01 17:07:32 +0530

    Delete src directory

--- src/components/ButtonLoadMore/Button.js
+++
@@ -1,9 +0,0 @@
-import { ButtonLoadMore, ButtonLoadMoreWrapper } from "./Button.styled";
-
-export default function Button({ onClick }) {
-  return (
-    <ButtonLoadMoreWrapper>
-      <ButtonLoadMore onClick={onClick}>Load More</ButtonLoadMore>
-    </ButtonLoadMoreWrapper>
-  );
-}
--- src/components/StarRating/Star.js
+++
@@ -1,5 +0,0 @@
-import { FullStar, EmptyStar } from "./Star.styled";
-
-export default function Star({ full, value }) {
-  return <>{full ? <FullStar value={value} /> : <EmptyStar value={value} />}</>;
-}
